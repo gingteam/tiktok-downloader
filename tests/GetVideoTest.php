c23b122c9738a9578be2e70a71ae7c05f540c035
ging-dev <thanh1101dev@gmail.com>
2023-10-04 19:12:39 +0700

    chore: add more test

--- tests/GetVideoTest.php
+++ tests/GetVideoTest.php
@@ -13,4 +13,5 @@ test('get video', function () {
     expect($video->get('https://www.tiktok.com/@tiin.vn/video/7106079259537591554?is_copy_url=1&is_from_webapp=v1'))->toBeString();
     $video->setDriver(new FacebookDriver());
     expect($video->get('https://www.facebook.com/watch/?v=343380744781609'))->toBeString();
+    expect($video->get('https://www.facebook.com/watch/?v=343380744781601'))->toBeFalse();
 });
